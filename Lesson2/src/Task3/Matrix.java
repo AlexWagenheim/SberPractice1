36b3d4f0faa4556f002e71378ee57ee1acb925be
AlexWagenheim <wagenheim.alex@yandex.ru>
2022-02-19 17:52:09 +0300

    Урок 2, задание 3 - выполнено

---
+++ Lesson2/src/Task3/Matrix.java
@@ -0,0 +1,90 @@
+package Task3;
+
+public class Matrix {
+    private double[][] a;
+    private int m, n;
+
+    Matrix(int m, int n) {
+        a = new double[m + 1][n + 1];
+        this.m = m;
+        this.n = n;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                a[i][j] = 0;
+            }
+        }
+    }
+
+    public double getCell(int i, int j) {
+        return a[i][j];
+    }
+
+    public void setCell(int i, int j, double x) {
+        a[i][j] = x;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                s = (String.format("%s %s", s, a[i][j]));
+            }
+            s = (String.format("%s\n", s));
+        }
+        return s;
+    }
+
+    static Matrix sum(Matrix A, Matrix B) {
+        Matrix C = new Matrix(A.m, A.n);
+        if ((A.m == B.m) && (A.n == B.n)) {
+            for (int i = 1; i <= C.m; i++) {
+                for (int j = 1; j <= C.n; j++) {
+                    C.setCell(i, j, A.getCell(i, j) + B.getCell(i, j));
+                }
+            }
+        }
+        else {
+            System.out.println(String.format("Размеры матриц %sx%s и %sx%s не совпадают", A.m, A.n, B.m, B.n));
+        }
+        return C;
+    }
+
+    static Matrix multiplyByNumber(double k, Matrix A) {
+        Matrix C = new Matrix(A.m, A.n);
+        for (int i = 1; i <= C.m; i++) {
+            for (int j = 1; j <= C.n; j++) {
+                C.setCell(i, j, k*A.getCell(i, j));
+            }
+        }
+        return C;
+    }
+    private static double getCellInMult(int x, int y, Matrix A, Matrix B) {
+        double result = 0;
+        for (int i = 1; i <= A.n; i++) {
+            result += A.getCell(x, i)*B.getCell(i, y);
+        }
+        return result;
+    }
+    static Matrix mult(Matrix A, Matrix B) {
+        Matrix C = new Matrix(A.m, B.n);
+        if (A.n == B.m) {
+            for (int i = 1; i <= C.m; i++) {
+                for (int j = 1; j <= C.n; j++) {
+                    C.setCell(i, j, getCellInMult(i, j, A, B));
+                }
+            }
+        }
+        else {
+            System.out.println(String.format("Недопустимые размеры матриц: %sx%s и %sx%s", A.m, A.n, B.m, B.n));
+        }
+        return C;
+    }
+}
---
+++ Lesson2/src/Task3/MatrixDriver.java
@@ -0,0 +1,47 @@
+package Task3;
+
+public class MatrixDriver {
+
+    private static void fillArray(Matrix A, int m, int n) {
+        int SegmentLength = 10;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                A.setCell(i, j, (int) (Math.random()*SegmentLength));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        double k = 10;
+        int m1 = 3;
+        int n1 = 4;
+        int m2 = 3;
+        int n2 = 4;
+        int m3 = 4;
+        int n3 = 3;
+
+        Matrix A = new Matrix(m1, n1);
+        fillArray(A, m1, n1);
+        System.out.println("A:");
+        System.out.println(A);
+
+        Matrix B = new Matrix(m2, n2);
+        fillArray(B, m2, n2);
+        System.out.println("B:");
+        System.out.println(B);
+
+        Matrix C = new Matrix(m3, n3);
+        fillArray(C, m3, n3);
+        System.out.println("C:");
+        System.out.println(C);
+
+        System.out.println("A + B:");
+        System.out.println(Matrix.sum(A, B));
+
+        System.out.println(String.format("k = %s; k*A:", k));
+        System.out.println(Matrix.multiplyByNumber(k, A));
+
+        System.out.println("A*C:");
+        System.out.println(Matrix.mult(A, C));
+    }
+}
